376f70cfa66971a5a1a1c7d5f1a6d88dea1edc82
Burak İpekci <burakipekci@outlook.com>
2023-07-31 10:29:42 +0300

    BCryptTest

---
+++ BCryptDemo/BCryptDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace BCryptDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Password:");
+            string password = Console.ReadLine(); 
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+            Console.WriteLine("Password: " + password);   
+            bool isPasswordCorrect = BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+            if (isPasswordCorrect)
+            {
+                Console.WriteLine("Hashed Password => " + " "+BCrypt.Net.BCrypt.HashPassword(password));
+            }
+    
+        }
+    }
+    }
+
